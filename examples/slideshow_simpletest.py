e854aeb0a85c688381b5626107b0430ebc13221d
Melissa LeBlanc-Williams <melissa@melissagirl.com>
2019-02-12 09:25:09 -0800

    Removed pylint from examples

--- examples/slideshow_simpletest.py
+++ examples/slideshow_simpletest.py
@@ -3,10 +3,8 @@ import pulseio
 from adafruit_slideshow import PlayBackOrder, SlideShow
 
 # Create the slideshow object that plays through once alphabetically.
-#pylint: disable=no-member
 slideshow = SlideShow(board.DISPLAY, pulseio.PWMOut(board.TFT_BACKLIGHT), folder="/",
                       loop=False, order=PlayBackOrder.ALPHABETICAL)
-#pylint: enable=no-member
 
 while slideshow.update():
     pass
--- examples/slideshow_touch.py
+++ examples/slideshow_touch.py
@@ -9,10 +9,8 @@ back_button = touchio.TouchIn(board.TOUCH1)
 brightness_up = touchio.TouchIn(board.TOUCH3)
 brightness_down = touchio.TouchIn(board.TOUCH2)
 
-#pylint: disable=no-member
 slideshow = SlideShow(board.DISPLAY, pulseio.PWMOut(board.TFT_BACKLIGHT), folder="/",
                       auto_advance=False, dwell=0)
-#pylint: enable=no-member
 
 while True:
     if forward_button.value:
