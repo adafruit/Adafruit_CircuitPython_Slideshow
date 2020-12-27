c5d72727c371ab88ecc3882d3dde052e106d076f
Limor "Ladyada" Fried <limor@ladyada.net>
2020-12-27 12:31:22 -0500

    Revert "Use `os.path.join` to concatenate paths"

--- adafruit_slideshow.py
+++ adafruit_slideshow.py
@@ -244,11 +244,11 @@ class SlideShow:
         # Load the image names before setting order so they can be reordered.
         self._img_start = None
         self._file_list = [
-            os.path.join(folder, f)
+            folder + f
             for f in os.listdir(folder)
             if (
                 not f.startswith(".")
-                and (f.endswith(".bmp") or _check_json_file(os.path.join(folder, f)))
+                and (f.endswith(".bmp") or _check_json_file(folder + f))
             )
         ]
 
